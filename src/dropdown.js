6b9c23be382b7b8bff50974604ceaa0e07347f7e
Heo <jhjjang0902@gmail.com>
2022-09-22 11:25:14 +0900

    add new function

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,9 +1,12 @@
 const themeDrops = document.getElementById("theme-drops");
+const locationDrops = document.getElementById("location-drops");
 
 const selectTheme = event => {
-    if(event.target.classList == "dropdown-item") {
+    if(event.target.classList.contains("theme-item")) {
     themeDrops.innerText = event.target.innerText;
+    } else if(event.target.classList.contains("location-item")) {
+    locationDrops.innerText = event.target.innerText;
     }
-};
+}
 
 window.addEventListener("click", selectTheme);
