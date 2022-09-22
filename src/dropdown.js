83946b4fa117f9ea4a372ed58879b05bda2dc8c1
Heo <jhjjang0902@gmail.com>
2022-09-22 10:11:56 +0900

    optimize codes

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,16 +1,9 @@
 const themeDrops = document.getElementById("theme-drops");
-const foodTheme = document.getElementById("food-theme");
-const healingTheme = document.getElementById("healing-theme");
-const shoppingTheme = document.getElementById("shopping-theme");
-const tourTheme = document.getElementById("tour-theme");
-
-const themeSelect = document.getElementById("theme-select");
 
 const selectTheme = event => {
+    if(event.target.classList == "dropdown-item") {
     themeDrops.innerText = event.target.innerText;
-}
+    }
+};
 
-foodTheme.addEventListener("click", selectTheme);
-healingTheme.addEventListener("click", selectTheme);
-shoppingTheme.addEventListener("click", selectTheme);
-tourTheme.addEventListener("click", selectTheme);
+window.addEventListener("click", selectTheme);
